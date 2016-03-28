03d7c41e29bab987aef42ae3b1c21ac2d0b2fc02
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-03-28 07:13:41 -0700

    Move functions to utils file

---
+++ utils.js
@@ -0,0 +1,187 @@
+#!/usr/bin/env node --harmony
+
+var fs = require('fs-extra');
+var chalk = require('chalk');
+var moment = require('moment');
+
+function getConfig() {
+  var config = process.env['HOME'] + '/.nonoterc.json';
+  // TODO: handle the case of `nonote new` when `nonote init` has not been run
+  return fs.readJsonSync(config).notesDirectory;
+}
+
+function getDir(type) {
+  var today = moment().format("DD-MM-YYYY");
+  var notesDir = getConfig();
+  var days = notesDir + '/days/';
+  var toDir = days + today;
+  return toDir;
+}
+
+function createDir(create, path) {
+  if (create) {
+    fs.mkdirsSync(path);
+    console.log(' ');
+    console.log(chalk.green('Success!'));
+  } else {
+    console.log(' ');
+    console.log('Awesome, make sure that dir exsists!');
+  }
+}
+
+function initializeNotes(userDir) {
+  var rcFile = process.env['HOME'] + '/.nonoterc.json';
+  fs.closeSync(fs.openSync(rcFile, 'w'));
+
+  var dotFileJSON = {}
+  dotFileJSON.notesDirectory = userDir;
+
+  fs.writeJsonSync(rcFile, dotFileJSON);
+  console.log(' ');
+  console.log(chalk.green('Success!'));
+  console.log(' ');
+  console.log('dotfile `.nonoterc.json` created at $HOME' );
+  console.log(' ');
+  console.log('Notes will be made in this directory: ')
+  console.log(chalk.cyan(dotFileJSON.notesDirectory));
+  console.log(' ');
+}
+
+// TODO: think about changing these status functions to a single type
+// signiture -> changeStatus(index, key, newStatus)
+// would this be a case for currying? or some other functional tecq?
+
+function makeNote(jsonObj) {
+  var dir = getDir();
+  var toMd = dir + '/note.md';
+  var noteMd = fs.createWriteStream(toMd);
+  Object.keys(jsonObj).map(function(title) {
+    noteMd.write("# " + title + "\n");
+    Object.keys(jsonObj[title]['items']).map(function(items, index){
+      var status = jsonObj[title]['items'][items]['status'];
+      var checkBox = '- [ ]';
+      var itemIndex = ' ' + index + '.) ';
+      if (status === 'complete') {
+        checkBox = '- [x]';
+      } else if (status === 'failed') {
+        checkBox = '- [-]';
+      }
+      noteMd.write(checkBox + itemIndex + jsonObj[title]['items'][items]['description'] + "\n");
+    });
+    noteMd.write("\n");
+  });
+}
+
+function addNote(noteObj, key) {
+  var dir = getDir();
+  var toData = dir + '/data.json';
+  var dataJSON = fs.readJsonSync(toData);
+  var noteString = noteObj.reduce(function(memo, word){
+    return memo + ' ' + word;
+  });
+
+  var descriptionObj = {
+    description: noteString,
+    status: 'incomplete',
+  };
+
+  // modify toData
+  Object.keys(dataJSON).map(function(note){
+    if (dataJSON[note]['cli-ref'] === key) {
+      dataJSON[note]['items'].push(descriptionObj);
+      fs.writeJsonSync(toData, dataJSON);
+    }
+  });
+  makeNote(dataJSON);
+}
+
+function removeNote(index, key) {
+  var dir = getDir();
+  var toData = dir + '/data.json';
+  var dataJSON = fs.readJsonSync(dir + '/data.json');
+  var cliFound = false;
+  Object.keys(dataJSON).map(function(note, noteIndex){
+    if (dataJSON[note]['cli-ref'] === key) {
+      cliFound = true;
+      if (!dataJSON[note]['items'][index]) {
+        throw new Error('index ' + index + ' in "' + key + '" object does not exist');
+      }
+      dataJSON[note]['items'].splice(index, 1);
+      fs.writeJsonSync(toData, dataJSON);
+    } else if (Object.keys(dataJSON).length === (noteIndex + 1) && !cliFound) {
+      throw new Error('"' + key + '" <cli-ref> does not exist');
+    }
+  });
+  makeNote(dataJSON);
+}
+
+function completeNote(index, key) {
+  var dir = getDir();
+  var toData = dir + '/data.json';
+  var dataJSON = fs.readJsonSync(toData);
+  var cliFound = false;
+  Object.keys(dataJSON).map(function(note, noteIndex) {
+    if (dataJSON[note]['cli-ref'] === key) {
+      cliFound = true;
+      if (!dataJSON[note]['items'][index]) {
+        throw new Error('index ' + index + ' in "' + key + '" object does not exist');
+      }
+      dataJSON[note]['items'][index]['status'] = 'complete';
+      fs.writeJsonSync(toData, dataJSON);
+    } else if (Object.keys(dataJSON).length === (noteIndex + 1) && !cliFound) {
+      throw new Error('"' + key + '" <cli-ref> does not exist');
+    }
+  });
+  makeNote(dataJSON);
+}
+
+function incompleteNote(index, key) {
+  var dir = getDir();
+  var toData = dir + '/data.json';
+  var dataJSON = fs.readJsonSync(toData);
+  var cliFound = false;
+  Object.keys(dataJSON).map(function(note, noteIndex) {
+    if (dataJSON[note]['cli-ref'] === key) {
+      cliFound = true;
+      if (!dataJSON[note]['items'][index]) {
+        throw new Error('index ' + index + ' in "' + key + '" object does not exist');
+      }
+      dataJSON[note]['items'][index]['status'] = 'incomplete';
+      fs.writeJsonSync(toData, dataJSON);
+    } else if (Object.keys(dataJSON).length === (noteIndex + 1) && !cliFound) {
+      throw new Error('"' + key + '" <cli-ref> does not exist');
+    }
+  });
+  makeNote(dataJSON);
+}
+
+function failNote(index, key) {
+  var dir = getDir();
+  var toData = dir + '/data.json';
+  var dataJSON = fs.readJsonSync(toData);
+  var cliFound = false;
+  Object.keys(dataJSON).map(function(note, noteIndex) {
+    if (dataJSON[note]['cli-ref'] === key) {
+      cliFound = true;
+      if (!dataJSON[note]['items'][index]) {
+        throw new Error('index ' + index + ' in "' + key + '" object does not exist');
+      }
+      dataJSON[note]['items'][index]['status'] = 'failed';
+      fs.writeJsonSync(toData, dataJSON);
+      return makeNote(dataJSON);
+    } else if (Object.keys(dataJSON).length === (noteIndex + 1) && !cliFound) {
+      throw new Error('"' + key + '" <cli-ref> does not exist');
+    }
+  });
+}
+
+module.exports = makeNote;
+module.exports = addNote;
+module.exports = removeNote;
+module.exports = completeNote;
+module.exports = incompleteNote;
+module.exports = failNote;
+module.exports = getDir;
+module.exports = initializeNotes;
+module.exports = getConfig;
+module.exports = createDir;
